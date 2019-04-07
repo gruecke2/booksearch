9ae9a1bf19d8d1376b7c83fd27ec51f152a32848
GarretR <garret.rueckert.guitar@gmail.com>
2019-04-06 19:10:33 -0600

    Added index into routes folder directly

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,5 @@ const bookRoutes = require("./books");
 
 // Book routes
 router.use("/books", bookRoutes);
-router.use((req, res) =>
- res.sendFile(path.join(__dirname, "../client/public/index.html"))
-);
+
 module.exports = router;
